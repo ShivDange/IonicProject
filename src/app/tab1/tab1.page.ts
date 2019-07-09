61f338959d5a095db29600c2087fc0983276d671
macmini <macmini@macminis-Mac-mini.local>
2019-07-09 17:07:03 +0530

    updated

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import { ActionSheetController } from '@ionic/angular';
+import { MenuController } from '@ionic/angular';
+
+
+
+
+
 
 @Component({
   selector: 'app-tab1',
@@ -6,7 +13,52 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  today;
 
-  constructor() {}
+  constructor(public actionSheetController: ActionSheetController , private menu: MenuController) {
+    this.today = new Date().toISOString();
+  }
+  openFirst() {
+    this.menu.enable(true, 'first');
+    this.menu.open('first');
+  }
 
+  async presentActionSheet(event: Event) {
+    const actionSheet = await this.actionSheetController.create({
+    
+      buttons: [{
+        text: 'Mark completed',
+        role: 'destructive',
+        // cssClass: 'actionButton',
+  
+        handler: () => {
+          console.log('Delete clicked');
+        }
+      }, {
+        text: 'Edit',
+        cssClass: 'actionButton',
+        handler: () => {
+          console.log('Share clicked');
+        }
+      }, {
+        text: 'Create New Task',
+        cssClass: 'actionButton',
+        
+        handler: () => {
+          console.log('Play clicked');
+        }
+      },  {
+        text: 'Cancel',
+        cssClass: 'cancelButton',
+       
+        role: 'cancel',
+        handler: () => {
+          console.log('Cancel clicked');
+        }
+      }]
+    });
+    
+    await actionSheet.present();
+    
+  }
 }
